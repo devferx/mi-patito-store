39252133c22346ba0ecd84b469ab9c3f47f2023a
devferx <ferkki@hotmail.com>
2025-04-12 19:16:02 -0400

    fix: add return types to getAllDucks and createDuck functions

--- frontend/src/services/ducks.service.ts
+++ frontend/src/services/ducks.service.ts
@@ -4,13 +4,13 @@ import type { Duck } from '@/models/duck'
 import type { GetAllDucksResponse } from '@/interfaces/get-all-ducks-response'
 import type { CreateDuckResponse } from '@/interfaces/create-duck-response'
 
-export const getAllDucks = async () => {
+export const getAllDucks = async (): Promise<Duck[]> => {
   const { data } = await api.get<GetAllDucksResponse>('/ducks')
   const { data: ducks } = data
   return ducks
 }
 
-export const createDuck = async (duck: Omit<Duck, 'id'>) => {
+export const createDuck = async (duck: Omit<Duck, 'id'>): Promise<Duck> => {
   const { data } = await api.post<CreateDuckResponse>('/ducks', duck)
   const { data: newDuck } = data
   return newDuck
