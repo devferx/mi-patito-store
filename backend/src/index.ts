0b1fcb987c76dda125c864ef44b3f9ea48affb48
devferx <ferkki@hotmail.com>
2025-04-12 18:25:56 -0400

    Add CORS support to the application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import duckRoutes from './routes/ducks.routes'
 
@@ -9,6 +10,7 @@ async function main() {
   const port = process.env.PORT || 3005
 
   app.use(express.json())
+  app.use(cors())
 
   app.use('/api/ducks', duckRoutes)
 
