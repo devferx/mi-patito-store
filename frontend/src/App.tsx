beabe9af134858119303a36bedf0d49a14fec85e
devferx <ferkki@hotmail.com>
2025-04-12 19:04:15 -0400

    feat: add shadcn config files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from './components/ui/button'
+
 export const App = () => {
   return (
     <main>
       <section className="container mx-auto py-20">
         <h2 className="text-center text-3xl font-bold">Almacen de Patitos</h2>
+        <div className="mt-5 flex flex-col items-center justify-center">
+          <Button>Click me</Button>
+        </div>
       </section>
     </main>
   )
