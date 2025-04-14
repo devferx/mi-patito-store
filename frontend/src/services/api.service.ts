cc3e0646865bd9a2619958e9a2f8bcac3173379b
devferx <ferkki@hotmail.com>
2025-04-14 11:07:12 -0400

    feat: add environment configuration for backend and frontend API URL

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-// TODO: Update the baseURL using the environment variable
-const baseURL = `http://localhost:3005/api`
+const isDev = import.meta.env.MODE === 'development'
+const baseURL =
+  import.meta.env.VITE_API_URL || (isDev ? 'http://localhost:3005/api' : '')
 
 export const api = axios.create({
   baseURL,
